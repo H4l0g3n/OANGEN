947153253d0fa4b0a52b4e4a5fcad12eacd51a5d
Christian <christiangeer123@outlook.com>
2020-12-23 15:22:32 -0500

    Iterates through pages, but weird error on last page I cant fix

--- OANNScraper.py
+++ OANNScraper.py
@@ -5,23 +5,46 @@ import time
 from getpass import getpass
 from time import sleep
 
-import content as content
+# import content as content
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, InvalidSessionIdException
 from selenium.webdriver import Chrome
 
 driver = Chrome(executable_path="C:/ChromeDriver/chromedriver.exe")
 driver.get("https://www.oann.com/category/newsroom/page/2/")
-
-headline_boxes = driver.find_elements_by_xpath('//*[@id="main-content"]/article[@*]/header/h3/a')
-headlines = []
-x = 0
-while x < 9:
-    headlines.append(headline_boxes[x].text)
-    print(headlines[x])
-    x += 1
+newsroom_page_count = 2
+newsroom_page_max = driver.find_element_by_xpath('//*[@id="main-content"]/div/a[5]').text
+int(newsroom_page_max)
 
 
+def extract_article_info(article_object):
+    headline = article_object.find_element_by_xpath('.//header/h3/a')
+    article_description = article_object.find_element_by_xpath('.//div[2]/p')
+    article_info = (headline.text, article_description.text)
+    return article_info
 
-# //*[@id="main-content"]/article[2]/header/h3/a
 
+data = []
+article_objects = driver.find_elements_by_xpath('//*[@id="main-content"]/article[@*]')
+for article_object in article_objects:
+    article_info = extract_article_info(article_object)
+    print(article_info)
+    data.append(article_info)
+
+while driver.find_element_by_xpath('//*[@id="main-content"]'):
+    article_objects = driver.find_elements_by_xpath('//*[@id="main-content"]/article[@*]')
+    for article_object in article_objects:
+        article_info = extract_article_info(article_object)
+        print(article_info)
+        data.append(article_info)
+        try:
+            next = driver.find_element_by_xpath('//*[@class="next page-numbers"]')
+            if next.is_displayed():
+                next.click()
+                newsroom_page_count += 1
+                print(newsroom_page_count)
+        except StaleElementReferenceException:
+            print(55 * "-")
+            driver.close()
+            #print(55 * "-")
+            #driver.close()
